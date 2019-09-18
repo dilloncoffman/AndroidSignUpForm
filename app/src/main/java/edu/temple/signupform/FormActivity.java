0026a5639c4eecbb643fd1196d5183a050054afa
Dillon Coffman <tug40394@temple.edu>
2019-09-18 10:37:28 -0400

    Gave each input an ID in order to be able to access the views in FormActivity; Set up empty OnClickListener for the Save button

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -2,12 +2,35 @@ package edu.temple.signupform;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
+
+    Button saveBtn;
+    EditText nameInput;
+    EditText emailInput;
+    EditText passInput;
+    EditText confirmPassInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        saveBtn = findViewById(R.id.saveBtn);
+        nameInput = findViewById(R.id.nameInput);
+        emailInput = findViewById(R.id.emailInput);
+        passInput = findViewById(R.id.passwordInput);
+        confirmPassInput = findViewById(R.id.confirmPassInput);
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
