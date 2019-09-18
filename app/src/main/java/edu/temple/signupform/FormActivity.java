91d4ba1f087527e8d36282308594c586d150480d
Dillon Coffman <tug40394@temple.edu>
2019-09-18 11:38:03 -0400

    Added error checking for empty fields and passwords that don't match, displaying a toast if either of those errors occur

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
@@ -29,7 +30,17 @@ public class FormActivity extends AppCompatActivity {
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // make sure every field has data entered
+                if (nameInput.length() == 0 || emailInput.length() == 0 || passInput.length() == 0 || confirmPassInput.length() == 0) {
+                    // if any field is empty display a Toast
+                    Toast.makeText(FormActivity.this , "Some field(s) is/are empty. Make sure to fill in all inputs.", Toast.LENGTH_SHORT).show();
+                }
+                // if password fields don't match
+                if (!(passInput.getText().toString().equals(confirmPassInput.getText().toString()))) {
+                    // display a toast letting the user know they don't match
+                    Toast.makeText(FormActivity.this , "Passwords do not match. Please be sure your password and password confirmation match.", Toast.LENGTH_SHORT).show();
+                }
+                // if all fields are complete and passwords match, display the user’s name inside a message
             }
         });
     }
